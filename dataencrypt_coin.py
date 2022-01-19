2f009b47f05294132cc906b930711fd3ddef423d
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2022-01-19 21:13:55 +0530

    add_transactions method added

--- dataencrypt_coin.py
+++ dataencrypt_coin.py
@@ -13,6 +13,10 @@ class Blockchain:
     def __init__(self):
         #empty list that will contain blocks
         self.chain = []
+        
+        #empty list that will contain transactions
+        self.transactions=[]
+        
         #genesis block - first block of blockchain
         #'0' in single quote as SHA-256 accepts only encoded strings
         self.create_block(proof = 1, previous_hash = '0')
@@ -25,8 +29,12 @@ class Blockchain:
         block = { 'index' : len(self.chain)+1 ,
                   'timestamp' : str(datetime.datetime.now()),
                   'proof' : proof,
-                   'previous_hash' : previous_hash
+                   'previous_hash' : previous_hash,
+                   'transactions' : self.transactions
                  }
+        
+        #once transactions are appended, transaction list will again come back to empty
+        self.transactions=[]
         
         #adding block in the chain of blocks
         self.chain.append(block)
@@ -79,6 +87,19 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+    
+    
+    def add_transaction(self,sender,receiver,amount):
+        '''This will help in adding transactions'''
+        self.transactions.append({
+            'sender' : sender,
+            'receiver' : receiver,
+            'amount' : amount         
+            })
+        
+        #this transaction must be added to next block, so getting previous block index
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
     
     
     
